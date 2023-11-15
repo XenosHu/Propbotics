62d2d1c0cd80b0c1b33ed5046afcf601bc1cbde1
XenosHu <121327330+XenosHu@users.noreply.github.com>
2023-11-15 14:03:22 -0500

    Update prop.py

--- prop.py
+++ prop.py
@@ -57,32 +57,45 @@ def clear_chat_history():
     st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
 st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
+def generate_gpt3_response(prompt_input):
+    response_content = ""
 
-if "apartment" in prompt_input.lower():
-    # # Generate SQL query using GPT-3.5 Turbo
-    # sql_query = generate_sql_query(prompt_input)
-    
+    if "apartment" in prompt_input.lower():
     # Connect to the database and execute the query
-    config = {
-        'user': 'propbotics',
-        'password': 'Propbotics123',
-        'host': 'chatbot.c0xmynwsxhmo.us-east-1.rds.amazonaws.com',
-        'database': 'chatbot',
-        'port': 3306
-    }
-    connection = mysql.connector.connect(**config)
-    # cursor = connection.cursor()
-    # cursor.execute(sql_query)
-    # query_results = cursor.fetchall()
+        config = {
+            'user': 'propbotics',
+            'password': 'Propbotics123',
+            'host': 'chatbot.c0xmynwsxhmo.us-east-1.rds.amazonaws.com',
+            'database': 'chatbot',
+            'port': 3306
+        }
+        connection = mysql.connector.connect(**config)
+        # cursor = connection.cursor()
+        # cursor.execute(sql_query)
+        # query_results = cursor.fetchall()
 
-    engine = create_engine(connection)
-    llm = OpenAI(temperature=0.5, model="gpt-3.5-turbo-16k")
-    service_context = ServiceContext.from_defaults(llm=llm)
-    engine = create_engine(connection)
-    
-    sql_database = SQLDatabase(engine)
-    inspector = inspect(engine)
-    table_names = inspector.get_table_names()
+        engine = create_engine(connection)
+        llm = OpenAI(temperature=0.5, model="gpt-3.5-turbo-16k")
+        service_context = ServiceContext.from_defaults(llm=llm)
+        engine = create_engine(connection)
+        
+        sql_database = SQLDatabase(engine)
+        inspector = inspect(engine)
+        table_names = inspector.get_table_names()
+
+        sql_query = chat_to_sql(prompt_input)
+
+        if sql_query:
+            try:
+                cursor.execute(sql_query)
+                query_results = cursor.fetchall()
+                response_content = format_query_results(query_results)
+            except Exception as e:
+                response_content = f"SQL Execution Error: {e}"
+        else:
+            response_content = "No valid SQL query generated."
+
+        return response_content
 
     # # Format and return the results
     # response_content = format_query_results(query_results)
@@ -175,12 +188,12 @@ def get_gpt3_response(prompt_input):
     return gpt_response.choices[0].message.content
     
 # User-provided prompt
+
 if prompt := st.chat_input(disabled=not openai_api_key):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.write(prompt)
 
-    # Generate a new response if last message is not from assistant
     if st.session_state.messages[-1]["role"] != "assistant":
         with st.chat_message("assistant"):
             with st.spinner("Thinking..."):
